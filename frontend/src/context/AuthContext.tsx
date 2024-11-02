643fcf2c23903c8db4d76861ec9899a2a7cf442c
JoaquinCatanzaritiTorrens <jcatanzariti@edu.tecnocampus.cat>
2024-11-02 17:55:34 +0100

    compilation errors solved

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect, ReactNode } from "react";
+import { createContext, useContext, useState, useEffect, ReactNode } from "react";
 
 // Definimos el tipo para el contexto de autenticación
 interface AuthContextType {

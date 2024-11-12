e2f5757adf69a1c7b04f5ff9de15e488f39c11db
BarrosoIsmael <122437646+BarrosoIsmael@users.noreply.github.com>
2024-11-12 19:11:58 +0100

    Scroll Button
    Botón que te dirige a la parte superior en caso de que te desplaces hacia abajo en el homepage.

---
+++ frontend/src/components/ScrollToTopButton.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { Fab, Zoom } from "@mui/material";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+
+const ScrollToTopButton: React.FC = () => {
+  const [show, setShow] = useState(false);
+
+  const handleScroll = () => {
+    setShow(window.scrollY > 100);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    <Zoom in={show}>
+      <Fab
+        color="primary"
+        size="small"
+        onClick={handleClick}
+        sx={fabStyle}
+        aria-label="scroll back to top"
+      >
+        <KeyboardArrowUpIcon />
+      </Fab>
+    </Zoom>
+  );
+};
+
+// Cambia la posición a la esquina inferior derecha
+const fabStyle = {
+  position: "fixed",
+  bottom: 16,
+  right: 16, // Cambiado a 'right' para posicionar a la derecha
+};
+
+export default ScrollToTopButton;

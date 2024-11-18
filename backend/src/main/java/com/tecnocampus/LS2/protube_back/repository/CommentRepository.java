f90f0b5174c274e9b635bd9565e6f73fcadf2316
JoaquinCatanzaritiTorrens <jcatanzariti@edu.tecnocampus.cat>
2024-11-18 16:59:31 +0100

    getCommentsByUsername

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/repository/CommentRepository.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/repository/CommentRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByVideoId(Long videoId);
+    List<Comment> findByUserId(Long userId);
 }

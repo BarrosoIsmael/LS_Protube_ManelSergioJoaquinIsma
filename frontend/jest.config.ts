1c10db5f4eb1c9e4f98ab83c13334fa24f70a334
JoaquinCatanzaritiTorrens <jcatanzariti@edu.tecnocampus.cat>
2024-12-06 17:25:32 +0100

    configs para tests añadidas

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,11 +1,22 @@
-import type {Config} from 'jest';
+import type { Config } from 'jest';
 
 const config: Config = {
   testEnvironment: "jsdom",
   setupFiles: ["<rootDir>/jest.polyfills.js"],
   testEnvironmentOptions: {
     customExportConditions: [''],
-  }
+  },
+  transform: {
+    '^.+\\.tsx?$': 'babel-jest',  // Use babel-jest to transform TypeScript files
+    '^.+\\.jsx?$': 'babel-jest',  // Use babel-jest for JavaScript/JSX files
+    '^.+\\.css$': 'jest-transform-stub',  // Handle CSS imports
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss)$': 'jest-transform-stub',  // Mock CSS files in tests
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!your-package-to-transform)"  // Optional, for transforming specific node_modules packages
+  ],
 };
 
 export default config;
--- frontend/src/utils/Env.ts
+++ frontend/src/utils/Env.ts
@@ -1,10 +1,23 @@
 export const getEnv = () => {
-  const { VITE_API_DOMAIN, VITE_MEDIA_DOMAIN, ...otherViteConfig } = import.meta.env;
+  if (process.env.JEST_WORKER_ID) {
+    return {
+      API_BASE_URL: 'http://localhost:8080/api',
+      FRONT_BASE_URL: 'http://localhost:8080',
+      __vite__: {}
+    };
+  }
+
+  /*const { VITE_API_DOMAIN, VITE_MEDIA_DOMAIN, ...otherViteConfig } = import.meta.env;
 
   return {
     API_BASE_URL: `${VITE_API_DOMAIN}/api`,
     FRONT_BASE_URL: `${VITE_MEDIA_DOMAIN}`,
     __vite__: otherViteConfig
+  };*/
+
+  return {
+    API_BASE_URL: 'http://localhost:8080/api',
+    FRONT_BASE_URL: 'http://localhost:8080',
+    __vite__: {}
   };
 };
-
